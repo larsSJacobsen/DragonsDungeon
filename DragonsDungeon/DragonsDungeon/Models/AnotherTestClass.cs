33e0180737c8fb18546d794b00a3b188f36afbf9
larsSJacobsen <larssiggaardjacobsen@gmail.com>
2016-05-23 19:47:35 +0200

    Yet Another Commit

---
+++ DragonsDungeon/DragonsDungeon/Models/AnotherTestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DragonsDungeon.Models
+{
+    public class AnotherTestClass : Controller
+    {
+        //
+        // GET: /AnotherTestClass/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
