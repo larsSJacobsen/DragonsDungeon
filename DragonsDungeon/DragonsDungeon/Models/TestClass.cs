233c81b86653bb0aca91178868f1b3b2df756f54
larsSJacobsen <larssiggaardjacobsen@gmail.com>
2016-05-23 19:31:09 +0200

    TestCommit of a class

---
+++ DragonsDungeon/DragonsDungeon/Models/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DragonsDungeon.Models
+{
+    public class TestClass : Controller
+    {
+        //
+        // GET: /TestClass/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
